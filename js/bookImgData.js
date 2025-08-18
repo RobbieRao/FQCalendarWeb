2bbb2f9851cf6d951460407b6edc212c18cdfa13
RobbieRao <61675305+RobbieRao@users.noreply.github.com>
2025-08-18 23:06:57 +0800

    Support 0-based image pages and auto size

--- js/bookImgData.js
+++ js/bookImgData.js
@@ -1,27 +1,27 @@
 // 书本目录，如果无ols变量则无目录，ols 变量名勿删，全局共享
-var ols = [
-  {
-    "caption": "测试目录1",
-    "page": "1",
-  },
-  {
-    "caption": "测试目录2",
-    "page": "2",
-  },
-  {
-    "caption": "测试目录3",
-    "page": "3",
-  },
-  {
-    "caption": "测试目录4",
-    "page": "4",
-  },
-];
+var ols = [];
+for (var i = 0; i <= 68; i++) {
+  ols.push({
+    caption: "页面 " + i,
+    page: String(i),
+  });
+}
 
 // 路径配置
-var loadImgpath = "./files/thumb/"
+var loadImgpath = "./files/thumb/";
 
-bookConfig.largePath = loadImgpath    // 大图路径
-bookConfig.normalPath = loadImgpath
-bookConfig.thumbPath = loadImgpath
-bookConfig.totalPageCount = ols.length    // 页面数量
+bookConfig.largePath = loadImgpath; // 大图路径
+bookConfig.normalPath = loadImgpath;
+bookConfig.thumbPath = loadImgpath;
+bookConfig.totalPageCount = ols.length; // 页面数量
+
+// 根据首张图片尺寸自适应页面大小
+(function () {
+  var img = new Image();
+  img.onload = function () {
+    bookConfig.largePageWidth = img.width;
+    bookConfig.largePageHeight = img.height;
+  };
+  img.src = loadImgpath + "0.png";
+})();
+
